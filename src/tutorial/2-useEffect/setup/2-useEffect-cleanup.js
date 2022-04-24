c22904831688fd271d974f0d8f20c175a9101cc9
praveen-1995 <praveenkumarthipparam@gmail.com>
2022-04-23 19:04:46 -0500

    useEffect Hook

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,10 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 // cleanup function
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = useState(window.innerWidth);
+
+  const changeSize = () => {
+    console.log("Changes to window size");
+    setSize(window.innerWidth);
+  };
+
+  //Before running useEffect again, it will return
+  useEffect(() => {
+    console.log("Useeffect Setup");
+    window.addEventListener("resize", changeSize);
+    return () => {
+      console.log("useeffect Cleanup");
+      window.removeEventListener("resize", changeSize);
+    };
+  });
+
+  return (
+    <>
+      <h1>Window Size</h1>
+      <h2>{size} px</h2>
+      {console.log("return in jsx")}
+    </>
+  );
 };
 
 export default UseEffectCleanup;
